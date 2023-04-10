4098fc8722ce2c8f124f4fc3f39107ada93171c6
Midjan Begić <midjan.begic@gmail.com>
2023-04-10 20:33:53 +0200

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import portret from '../assets/svg/about/portret.svg'
 
 export const headerData = {
     name: 'Midjan Begic',
     title: "Frontend developer",
     desciption:"Strive for perfection in everything you do. Take the best that exists and make it better. When it does not exist, design it.",
-    image: 'https://scontent-sof1-1.xx.fbcdn.net/v/t1.15752-9/309444879_398436888978807_4936952760036845207_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=r7dQYViCjTIAX8Emjtm&_nc_oc=AQl357TIbJ9FtnqAxr1kVqpskqfUe8B2mdI1uVsdhLx-jqjZf0mvjGsfYMmNXGkUaAY&_nc_ht=scontent-sof1-1.xx&oh=03_AVK67p8d4Uf40FwR04qZuGQpHH6Yv9LVi3RvK8O888zYzg&oe=63653A98',
+    image: portret,
     resumePdf: resume
 }
